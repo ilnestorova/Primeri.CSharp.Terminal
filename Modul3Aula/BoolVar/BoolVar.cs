d05d574d076c9fb07c520e19d8aeedac5a69bc37
ilnestorova <iliana.nestorova@gmail.com>
2016-07-18 14:13:31 +0300

    Modil039 Video06 Condition IF

--- Modul3Aula/BoolVar/BoolVar.cs
+++ Modul3Aula/BoolVar/BoolVar.cs
@@ -21,17 +21,32 @@ namespace BoolVar
             //Console.WriteLine( check1);
             //Console.WriteLine(check2);
             //Console.WriteLine(i[ui-1]);
-            bool exp1 = check1 && check2; //Logical "and"
-            bool exp2 = check1 || check2; //Logical "or"
-            bool exp3 = !check1; //Logical "not"
+            //bool exp1 = check1 && check2; //Logical "and"
+            //bool exp2 = check1 || check2; //Logical "or"
+            //bool exp3 = !check1; //Logical "not"
 
-            Console.WriteLine("Logical AND of "+check1.ToString()+" and "+check2.ToString()
-                +" is : " +exp1.ToString());
-            Console.WriteLine("Logical OR of "+check1.ToString()+" and "+check2.ToString()
-                +" is : "+exp2.ToString());
-            Console.WriteLine("Logical Negation of "+check1.ToString()+" is : "
-                +exp3.ToString());
-
+            //Console.WriteLine("Logical AND of "+check1.ToString()+" and "+check2.ToString()
+            //   +" is : " +exp1.ToString());
+            //Console.WriteLine("Logical OR of "+check1.ToString()+" and "+check2.ToString()
+            //   +" is : "+exp2.ToString());
+            //Console.WriteLine("Logical Negation of "+check1.ToString()+" is : "
+            //   +exp3.ToString());
+            if (ui <= i.Length&&ui>0)
+            {
+                Console.WriteLine(i[ui-1]);
+            }
+            else
+            {
+                if(ui<0)
+                {
+                    Console.WriteLine("Unvalid index");
+                }
+                if(ui>i.Length)
+                {
+                    Console.WriteLine("The index is bigger than the lenght of the array");
+                }
+                
+            }
         }
     }
 }
