59dbbb32a8e882a4ea27fbcb3d98f473173249b2
ilnestorova <iliana.nestorova@gmail.com>
2016-07-18 15:21:50 +0300

    Modil03 Video07 TryParse Method

---
+++ Modul3Aula/TryParse/TryParse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    class TryParse
+    {
+        static void Main(string[] args)
+        {
+            int[] i = new[] { 4, 3, 2 };
+            Console.WriteLine("Enter tne integer number");
+            int ui = 0; //Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+            bool check0 = int.TryParse(input, out ui);
+            bool check1 = ui <= i.Length;
+            bool check2 = ui <0;
+
+            if (check0 && check1 && check2)
+            {
+                Console.WriteLine(i[ui - 1]);
+            }
+            else
+            {
+                if (!check2)
+                {
+                    Console.WriteLine("Unvalid index");
+                }
+                if (!check1)
+                {
+                    Console.WriteLine("The index is bigger than the lenght of the array");
+                }
+                if (!check0)
+                {
+                    Console.WriteLine("The index must be integer  number");
+                }
+            }
+        }
+    }
+}
