b6ef21097aa744d5db97ae7337db08e90fe7c426
ilnestorova <iliana.nestorova@gmail.com>
2016-07-20 12:18:58 +0300

    Modul 3 Video9 Switch-case

--- Modul3Aula/ConditionalOperator/ConditionalOperator.cs
+++ Modul3Aula/ConditionalOperator/ConditionalOperator.cs
@@ -16,11 +16,27 @@ namespace ConditionalOperator
            
             string input = Console.ReadLine();
             bool check1 = int.TryParse(input, out ui);
+            /*
             ui = ui <= i.Length ? ui : 3;
             ui = ui >0 ? ui : 1;
             if(check1)
             {
                 Console.WriteLine(i[ui-1]);
+            }
+            */
+            switch(ui)
+            {
+                case 1: Console.Write("The first element of the array is: ");
+                    Console.WriteLine(i[ui-1]);break;
+                case 2:
+                    Console.Write("The second element of the array is: ");
+                    Console.WriteLine(i[ui - 1]); break;
+                case 3:
+                    Console.Write("The third element of the array is: ");
+                    Console.WriteLine(i[ui - 1]); break;
+                default:
+                    Console.Write("There are not such element in the array.Please, try with number between 1 and 3 ");
+                    Console.WriteLine(); break;
             }
         }
     }
