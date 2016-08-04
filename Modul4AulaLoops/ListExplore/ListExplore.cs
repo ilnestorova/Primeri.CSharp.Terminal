a57615a6a69512d15286433fdef0f26c4eaf9891
ilnestorova <iliana.nestorova@gmail.com>
2016-08-04 12:19:48 +0300

    Modul4 Video7 List

---
+++ Modul4AulaLoops/ListExplore/ListExplore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExplore
+{
+    class ListExplore
+    {
+        static void Main(string[] args)
+            //List
+            
+        {
+            string userinput = "";
+            List<int> mylist = new List<int>();
+            //Add to List
+            mylist.Add(1);
+            mylist.Add(3);
+            mylist.Add(5);
+            do
+            {
+                Console.Write("Please enter smtg: ");
+                userinput = Console.ReadLine();
+                
+                //Preview of List
+                if (userinput.Contains("show"))
+                {
+                    Console.Write("List: ");
+                    for (int i = 0; i < mylist.Count; i++)
+                    {
+                        Console.Write(mylist[i]);
+                        if (i != mylist.Count - 1) Console.Write(", ");
+                    }
+                    Console.WriteLine();
+                }
+                
+                //Size of List
+
+            } while (userinput != "exit");
+        }
+    }
+}
