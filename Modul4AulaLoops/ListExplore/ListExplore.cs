223ed6e0aa749d2dccc6bf26850290fada3818e7
ilnestorova <iliana.nestorova@gmail.com>
2016-08-05 11:44:55 +0300

    Modul4 Video8 ExampleList

--- Modul4AulaLoops/ListExplore/ListExplore.cs
+++ Modul4AulaLoops/ListExplore/ListExplore.cs
@@ -9,12 +9,15 @@ namespace ListExplore
     class ListExplore
     {
         static void Main(string[] args)
+            
             //List
             
         {
+            
             string userinput = "";
             List<int> mylist = new List<int>();
-            //Add to List
+            //Add to the List another values
+            // add <int>
             mylist.Add(1);
             mylist.Add(3);
             mylist.Add(5);
@@ -22,9 +25,25 @@ namespace ListExplore
             {
                 Console.Write("Please enter smtg: ");
                 userinput = Console.ReadLine();
+                if(userinput.ToLower().Contains("add"))
+                {
+                    try
+                    {
+                        int myadd = 0;
+                        if (int.TryParse(userinput.Split(' ')[1], out myadd))
+                            {
+                            mylist.Add(myadd);
+                        }
+                        Console.WriteLine();
+                    }
+                
+                    catch {
+                        Console.WriteLine("Please anter one interval between add and the integer");
+                    }
+                }
                 
                 //Preview of List
-                if (userinput.Contains("show"))
+                if (userinput.ToLower().Contains("show"))
                 {
                     Console.Write("List: ");
                     for (int i = 0; i < mylist.Count; i++)
@@ -32,12 +51,28 @@ namespace ListExplore
                         Console.Write(mylist[i]);
                         if (i != mylist.Count - 1) Console.Write(", ");
                     }
-                    Console.WriteLine();
+                    Console.WriteLine("\n");
                 }
-                
+                if(userinput.ToLower().Contains("size"))
+                {
+                    Console.WriteLine("The size of the mylist is: "+mylist.Count.ToString()+"\n");
+                }
                 //Size of List
 
             } while (userinput != "exit");
+            
+          /*  List<string> z = new List<string>();
+            z.Add("apple");
+            z.Add("pear");
+            z.Add("marmalade");
+            z.Add("grapes");
+            int i = z.IndexOf("marmalade");
+            z.RemoveAt(i);
+            foreach(string x in z)
+            {
+                Console.WriteLine(x);
+            }
+            */
         }
     }
 }
