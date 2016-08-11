d9f5b3405a21ac1160eb8f635d77945f5e9bcd18
ilnestorova <iliana.nestorova@gmail.com>
2016-08-11 14:12:43 +0300

    Modul4 Video9 Foreach

---
+++ Modul4AulaLoops/ForeachLoop/ForeachLoop.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachLoop
+{
+    class ForeachLoop
+    {
+        static void Main(string[] args)
+        // има за цел да извади срички от най-популярните брандове на планетата. 
+        //След това ще се опита да създаде нови брандове, като комбинира тези срички.
+        {
+            List<string> brands = new List<string>() { 
+                //Това е краткия начин по 
+                //който да дефинираме и запълним лист
+"sony","cocacola","pepsi","ibm","mentos","panasonic","fanta",
+"sprite","apple","microsoft","google","mcdonalds","samsung",
+"facebook","disney","pixar","amazon"}; //Лист с популярни брандове
+
+            List<string> srichki = new List<string>(); 
+            // Ще извадим срички от брандовете и ще ги сложим тук
+            int i = 0;
+
+            List<string> glasni = new List<string>() { "a", "e", "i", "o", "u", "y" };
+            // Тук ще държим гласните
+
+            foreach (string brand in brands)
+            {
+
+                for (i = 0; i < brand.Length - 1; i++)
+                {
+                    if (glasni.Contains(brand[i].ToString()) ||  
+                        // Ако сричката няма гласна, то ще я отхвърлим
+                        glasni.Contains(brand[i + 1].ToString()))
+                        srichki.Add("" + brand[i] + brand[i + 1]); 
+                    // Всички останали срички ги запомняме
+                }
+
+            }
+
+            Console.WriteLine("Открити срички: " + string.Join(" ", srichki) + "\n"); 
+            // Ще покажем сричките на екрана
+
+            var r = new Random();
+
+            string my_brand = "";  
+            // Тук ще пишем произволни срички
+            Console.WriteLine("Нови брандове:");
+            for (int k = 0; k < 20; k++)
+            { // Искаме да създадем 20 нови бранда
+                my_brand = ""; // Всеки път трябва да започваме от нищо
+                for (int j = 0; j < r.Next(2, 10); j++)
+                { // Искаме новия бранд да е с от 2 до 10 срички
+
+                    my_brand += srichki[r.Next(0, srichki.Count - 1)];
+                    // Добравяме произволна сричка към новия бранд
+                }
+                Console.WriteLine(my_brand); 
+                // Показваме новия бранд на екрана
+            }
+
+        }
+    }
+}
--- Modul4AulaLoops/ListExplore/ListExplore.cs
+++ Modul4AulaLoops/ListExplore/ListExplore.cs
@@ -9,11 +9,11 @@ namespace ListExplore
     class ListExplore
     {
         static void Main(string[] args)
-            
-            //List
-            
+
+        //List
+
         {
-           /* 
+
             string userinput = "";
             List<int> mylist = new List<int>();
             //Add to the List another values
@@ -25,62 +25,76 @@ namespace ListExplore
             {
                 Console.Write("Please enter smtg: ");
                 userinput = Console.ReadLine();
-                if(userinput.ToLower().Contains("add"))
+                if (userinput.ToLower().Contains("add"))
                 {
                     try
                     {
                         int myadd = 0;
                         if (int.TryParse(userinput.Split(' ')[1], out myadd))
-                            {
+                        {
                             mylist.Add(myadd);
                         }
                         Console.WriteLine();
                     }
-                
-                    catch {
+
+                    catch
+                    {
                         Console.WriteLine("Please anter one interval between add and the integer");
                     }
                 }
-                
+
                 //Preview of List
                 if (userinput.ToLower().Contains("show"))
                 {
                     Console.Write("List: ");
-                    for (int i = 0; i < mylist.Count; i++)
+                    foreach (int value in mylist)
+                    {
+                        Console.Write(value.ToString() + ", ");
+                    }
+
+                    /*
+                     for (int i = 0; i < mylist.Count; i++)
                     {
                         Console.Write(mylist[i]);
                         if (i != mylist.Count - 1) Console.Write(", ");
                     }
+                    */
                     Console.WriteLine("\n");
                 }
+                /*
                 if(userinput.ToLower().Contains("size"))
                 {
                     Console.WriteLine("The size of the mylist is: "+mylist.Count.ToString()+"\n");
                 }
                 //Size of List
+                */
 
             } while (userinput != "exit");
-           */ 
-           List<string> z = new List<string>();
-            z.Add("apple");
-            z.Add("pear");
-            z.Add("marmalade");
-            z.Add("grapes");
-            /*int i = z.IndexOf("marmalade");
-            z.RemoveAt(i);
-            foreach(string x in z)
-            {
-                Console.WriteLine(x);
-            }
-            */
-            z.Add("apricot");
-            z.Add("oringe");
-            z.Add("tree");
-            z.Sort();
-            Console.WriteLine(string.Join(",",z));
-            z.Reverse();
-            Console.WriteLine(string.Join(",", z));
+           /*
+                  List<string> z = new List<string>();
+                   z.Add("apple");
+                   z.Add("pear");
+                   z.Add("marmalade");
+                   z.Add("grapes");
+                 
+                    * int i = z.IndexOf("marmalade");
+                   z.RemoveAt(i);
+                   foreach(string x in z)
+                   {
+                       Console.WriteLine(x);
+                   }
+               
+                   z.Add("apricot");
+                   z.Add("oringe");
+                   z.Add("tree");
+                   z.Sort();
+                   Console.WriteLine(string.Join(",",z));
+                   z.Reverse();
+                   Console.WriteLine(string.Join(",", z));
+                   */
+                   
 
-        }
+           
+      }
     }
 }
