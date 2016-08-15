8a2b12d7b4846a4d69523e4d0d8eb737eea4e2d6
ilnestorova <iliana.nestorova@gmail.com>
2016-08-15 16:42:45 +0300

    Modul5 - Video3MethodsShuffleArrayInt&String]

--- Modul05Aula/MethodsAndArrays/MethodsAnrArrays.cs
+++ Modul05Aula/MethodsAndArrays/MethodsAnrArrays.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MethodsAndArrays
+/*namespace MethodsAndArrays
 {
     class MethodsAnrArrays
     {
@@ -51,4 +51,57 @@ namespace MethodsAndArrays
                
         }
     }
-}
+}*/
+
+namespace TestovaPrograma
+{
+
+    class MainClass
+    {
+
+        static Random _random = new Random();
+        // (1) Дефинираме и инициализираме променливата _random с генератор
+        // на произволни числа. Така може да го ползваме от всички методи.
+
+        public static void Main(string[] args)
+        {
+            int[] arint = { 1, 2, 3, 4, 5, 6 };
+            string[] arstr = { "first", "second", "third", "fourth","fifth","sixth" };
+
+            Shuffle(arint); // (3) Не използваме ref, но метода
+                         // пак сменя стойността на ar
+
+            Console.WriteLine(string.Join(" ", arint));
+
+            Shuffle(arstr);
+            Console.WriteLine(string.Join(" ", arstr));
+        }
+
+
+        // Метода за размешване 'shuffle'
+        // (2) Забележете как дефинираме метода. Ще свърши същата работа
+        // както "ref", но няма да има значение дали му подаваме масив от
+        // цели числа, масив от текстови низове или друг вид масив.
+
+        static void Shuffle<T>(T[] myar)
+        {
+            /*int n = myar.Length;
+            for (int i = 0; i < n; i++)
+            {
+                // Алгоритъм за размешване 'Fisher-Yates'
+                int r = i + (int)(_random.NextDouble() * (n - i));
+                T t = myar[r];
+                myar[r] = myar[i];
+                myar[i] = t;*/
+                for(int i=myar.Length-1;i>0;i--)
+            {
+                int index = _random.Next(i);
+                T t = myar[index];
+                myar[index] = myar[i];
+                myar[i] = t;
+                
+            }
+            }
+        }
+    }
+
